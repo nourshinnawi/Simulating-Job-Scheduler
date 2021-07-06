e40555efd45c44bc7cbb3a2f3ce4493b5e7684f4
nnnn <50388570+nourshinnawi@users.noreply.github.com>
2021-07-05 18:32:52 -0700

    Create Jobs.h

---
+++ Jobs.h
@@ -0,0 +1,15 @@
+//Jobs
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Has variables for the jobs
+struct Jobs
+{
+	string name;
+	int burst;
+	int start;
+	int end;
+};
